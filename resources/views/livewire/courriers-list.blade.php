9f2a0d5e32c294cbbe9f95bee72baea02adffa26
Ahmedou Khlil <ouldkhlil09@gmail.com>
2025-03-08 04:19:04 +0000

    14eme commit

--- resources/views/livewire/courriers-list.blade.php
+++ resources/views/livewire/courriers-list.blade.php
@@ -143,7 +143,12 @@
                             <div class="flex space-x-2">
                                     
                                 </a>
-                                    
+                                <a href="{{ route('courriers.show', $courrier) }}" class="text-blue-600 hover:text-blue-900" title="Afficher">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+            </svg>
+        </a>
                                 </a>
                                 <button wire:click="deleteConfirm({{ $courrier->id }})" class="text-red-600 hover:text-red-900" title="Supprimer">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
